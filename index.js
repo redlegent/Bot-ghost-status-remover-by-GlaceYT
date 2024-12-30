4881cb02c8a565f0a27e6bcc4f72ca3a32ed8f67
redlegent <158600932+redlegent@users.noreply.github.com>
2024-12-30 17:30:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["No Cfx❗","Unbanned❗","Unbanned❗"," No Cfx❗","Unbanned❗"];
+const statusMessages = ["Crown Solutions👑 ","Cfx Remover❗","Crown Solutions❗"," Cfx Remover❗","AntiFiveM🤡"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
