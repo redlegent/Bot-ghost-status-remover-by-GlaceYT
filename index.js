4222b7d4c6f7e0468d5c151560e8e6dfba5b5995
redlegent <158600932+redlegent@users.noreply.github.com>
2025-01-01 20:54:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["🎧 Listening to Haze Shop", "🎮 Playing Haze Shop"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
